f951213941cd71e2ecfcebffd130c09b00efa7f1
wsb8618 <55951156+wsb8618@users.noreply.github.com>
2022-03-14 17:33:55 +0900

    [추가] 주석 추가
    주석 추가

--- Assets/Scripts/Character/CharacterHealthSlider.cs
+++ Assets/Scripts/Character/CharacterHealthSlider.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class CharacterHealthSlider : MonoBehaviour
 {
+    // 인게임 내의 캐릭터 상단 체력바 UI와 게임화면 UI의 체력바 동기화
+    
     public Slider characterSlider3D;
     Slider characterSlider2D;
 

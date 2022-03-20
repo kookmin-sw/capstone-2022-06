b2a2f6ff7266c03f4aef3342d59d06462f616f49
namgyeongtae <nrudxo12@kookmin.ac.kr>
2022-03-20 21:00:22 +0900

    Add Select UI and SelectManager script
    add select ui and script
    
    see also :  #23

---
+++ Assets/Scripts/NetworkScripts/SelectManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class SelectManager : MonoBehaviourPunCallbacks
+{
+    string clickImgName;
+
+    [SerializeField]
+    List<GameObject> playersPick;
+
+    PhotonView PV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clickImgName != null)
+            PV.RPC("ChangeIcon", RpcTarget.All, clickImgName);
+    }
+
+    public void findClickImg()
+    {
+        GameObject clickObj = EventSystem.current.currentSelectedGameObject;
+
+        clickImgName = clickObj.name;
+    }
+
+    [PunRPC]
+    void ChangeIcon(string name)
+    {
+        Sprite iconImg = Managers.Resource.Load<Sprite>($"Private/Textures/Character Portraits/{name}");
+
+        playersPick[PV.ViewID - 1].GetComponent<Image>().sprite = iconImg;
+    }
+}

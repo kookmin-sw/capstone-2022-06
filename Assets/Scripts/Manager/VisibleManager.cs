9225419022e58931b9c7d27f552911b53570b070
HOGYEONG CHOE <devherdson@gmail.com>
2022-03-23 22:18:54 +0900

    [추가] VisibleManager 추가
    아군에게 보이는 적을 관리하는 매니저 추가
    
    관련 이슈 : #24

---
+++ Assets/Scripts/Manager/VisibleManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibleManager
+{
+    Dictionary<GameObject, int> visibleObjects = new Dictionary<GameObject, int>();
+
+    void AddVisible(GameObject unit)
+    {
+        if (visibleObjects.ContainsKey(unit))
+        {
+            ++visibleObjects[unit];
+        }
+        else
+        {
+            Renderer renderer = unit.GetComponent<Renderer>();
+            if (renderer)
+            {
+                renderer.enabled = true;
+            }
+        }
+    }
+
+    void SubtractVisible(GameObject unit)
+    {
+        if (!visibleObjects.ContainsKey(unit))
+        {
+            return;
+        }
+
+        --visibleObjects[unit];
+
+        if (visibleObjects[unit] <= 0)
+        {
+            Renderer renderer = unit.GetComponent<Renderer>();
+            renderer.enabled = false;
+            visibleObjects.Remove(unit);
+        }
+    }
+}

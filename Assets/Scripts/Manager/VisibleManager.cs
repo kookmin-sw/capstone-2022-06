af8784f0b8cea71f7988cf71274a1f98fadfbbf6
HOGYEONG CHOE <devherdson@gmail.com>
2022-03-24 00:05:45 +0900

    [변경] VisibleManager에 주석 추가
    VisibleManager에 주석 추가

--- Assets/Scripts/Manager/VisibleManager.cs
+++ Assets/Scripts/Manager/VisibleManager.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class VisibleManager
 {
+    /*
+    아군에게 스캔된 유닛이 몇 번 스캔되었는지 관리하는 딕셔너리
+    예를 들어 value가 2면 아군에게 2번 스캔된 오브젝트임
+    */
     Dictionary<GameObject, int> visibleObjects = new Dictionary<GameObject, int>();
 
+    /*
+    인자로 들어온 게임 오브젝트를 visibleObjects value에 1 추가하고 1 이상이면 enabled를 true로 바꿈
+    */
     public void AddVisible(GameObject unit)
     {
         if (visibleObjects.ContainsKey(unit))
@@ -22,6 +29,9 @@ public class VisibleManager
         }
     }
 
+    /*
+    인자로 들어온 게임 오브젝트를 visibleObjects value에 1 빼고 0 이하면 enabled를 false로 바꿈
+    */
     public void SubtractVisible(GameObject unit)
     {
         if (!visibleObjects.ContainsKey(unit))

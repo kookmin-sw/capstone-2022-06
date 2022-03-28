e976e1c6755ec4bf80ae1f0ba69ce45ff4a90194
HOGYEONG CHOE <devherdson@gmail.com>
2022-03-28 23:40:18 +0900

    [추가] 편의 기능을 담는 클래스 Utiil 추가
    편의 기능 메서드로 묶은 Util 클래스 추가 및 자식 컴포넌트를 찾는 메서드 추가
    
    관련 이슈 : #24

---
+++ Assets/Scripts/ExLibrary/Util.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util
+{
+    public static T SearchChild<T>(GameObject go, string name = null, bool pushdown = false) where T : UnityEngine.Object
+    {
+        if (!go)
+        {
+            return null;
+        }
+
+        if (pushdown)
+        {
+            foreach(T e in go.GetComponentsInChildren<T>())
+            {
+                if (string.IsNullOrEmpty(name) || e.name == name)
+                {
+                    return e;
+                }
+            }
+        }
+        else
+        {
+            foreach(Transform child in go.transform)
+            {
+                if (string.IsNullOrEmpty(name) || child.name == name)
+                {
+                    T ret = child.GetComponent<T>();
+                    if (ret)
+                    {
+                        return ret;
+                    }
+                }
+            }
+        }
+        
+
+        return null;
+    }
+}

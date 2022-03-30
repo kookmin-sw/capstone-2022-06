cca4fc18def07a37da68b287bb0698c2bef3090a
HOGYEONG CHOE <devherdson@gmail.com>
2022-03-31 02:00:13 +0900

    [변경] Managers에 UI추가
    Managers에서 UIManger 호출할 수 있도록 변경
    
    관련 이슈 : #24

--- Assets/Scripts/Manager/Managers.cs
+++ Assets/Scripts/Manager/Managers.cs
@@ -9,9 +9,11 @@ public class Managers : MonoBehaviour
 
     ResourceManager _resource = new ResourceManager();
     VisibleManager _visible = new VisibleManager();
+    UIManager _ui = new UIManager();
 
     public static ResourceManager Resource { get { return Instance._resource; } }
     public static VisibleManager Visible { get { return Instance._visible; }}
+    public static UIManager UI {get { return Instance._ui; }}
 
     // Start is called before the first frame update
     void Start()

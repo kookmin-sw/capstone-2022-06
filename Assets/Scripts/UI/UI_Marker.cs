4952febda0c4845bc3c0b545dfcefcd6d2a1ab14
HOGYEONG CHOE <devherdson@gmail.com>
2022-03-31 02:34:44 +0900

    [변경] UI_Marker의 Init() 호출시 UI의 위치를 부모 위에 놓이도록 함
    UI_Maker에서 Init()을 호출하면 UI의 위치가 부모 위에 놓이도록 변경
    
    관련 이슈 : #24

--- Assets/Scripts/UI/UI_Marker.cs
+++ Assets/Scripts/UI/UI_Marker.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI;
 
 public class UI_Marker : UI_Base
 {
+    enum GameObjects
+    {
+        Marker
+    }
+
     public override void Init()
     {
-        this.Bind<GameObject>(typeof(GameObjects));
+        Bind<GameObject>(typeof(GameObjects));
+        transform.position = transform.parent.position + new Vector3(0, 9, 0);
         // SetMarkerColor(new Vector3(255, 0, 0));
     }
 
-    void OnEnable()
+    void Start()
     {
         Init();
     }
@@ -23,13 +29,8 @@ public class UI_Marker : UI_Base
 
     void SetMarkerColor(Vector3 color)
     {
-        GameObject marker = this.Get<GameObject>((int)GameObjects.Marker);
+        GameObject marker = Get<GameObject>((int)GameObjects.Marker);
         Image markerImage = marker.GetComponent<Image>();
-        markerImage.color = new Color32((byte)color.x, (byte)color.y, (byte)color.z, 0);
-    }
-
-    enum GameObjects
-    {
-        Marker
+        markerImage.color = new Color32((byte)color.x, (byte)color.y, (byte)color.z, 255);
     }
 }

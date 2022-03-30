3183cb2923291371f86f04e136d739b130dd75b2
HOGYEONG CHOE <devherdson@gmail.com>
2022-03-30 23:17:12 +0900

    [추가] UI 매니저 추가
    UI를 관리하는 매니저 추가.
    커밋 시점에는 캔버스를 새로 붙이는 SetCanvas, 오브젝트에 월드 UI를 붙이는 AttachWorldUI, 그리고 미니맵 카메라에만 보여지는 UI를 붙이는 AttachMapMarker가 구현됨
    
    관련 이슈 : #24

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager
+{
+    // 캔버스 정렬 우선순위
+    int _order = 10;
+
+    /*
+    게임 오브젝트에 캔버스를 부착함
+    */
+    public void SetCanvas(GameObject go, bool ordering = true)
+    {
+        if (go is null)
+        {
+            return;
+        }
+
+        Canvas canvas = go.GetOrAddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.overrideSorting = true;
+        canvas.sortingOrder = (ordering ? _order++ : 0);
+    }
+
+    /*
+    월드 공간에 존재하는 오브젝트에 UI를 부착함
+    */
+    public T AttachWorldUI<T>(Transform parent = null, string name = null) where T : UI_Base
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            name = typeof(T).Name;
+        }
+
+        GameObject ui = Managers.Resource.Instantiate($"UI/WorldSpace/{name}");
+
+        if (ui is null)
+        {
+            return null;
+        }
+
+        if (parent is not null)
+        {
+            ui.transform.SetParent(parent);
+        }
+        
+        Canvas canvas = ui.GetOrAddComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+        canvas.worldCamera = Camera.main;
+
+        return ui.GetOrAddComponent<T>();
+    }
+
+    /*
+    미니맵에 보여질 UI 마커를 전용 카메라에 보이도록 부착함
+    */
+    public T AttachMapMarker<T>(Transform parent = null, string name = null) where T : UI_Base
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            name = typeof(T).Name;
+        }
+
+        GameObject ui = Managers.Resource.Instantiate($"UI/WorldSpace/{name}");
+
+        if (ui is null)
+        {
+            return null;
+        }
+
+        if (parent is not null)
+        {
+            ui.transform.SetParent(parent);
+        }
+
+        Canvas canvas = ui.GetOrAddComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+        canvas.worldCamera = Util.FindCamera("MinimapCamera");
+        return ui.GetOrAddComponent<T>();
+    }
+}

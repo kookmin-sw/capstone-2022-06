393168a68587541f7441bf7684f75d05a54b35fb
Suyeon Lee <baekgu1011@gmail.com>
2022-04-08 12:59:28 +0900

    [변경] 구매, 판매 기능 안정화

--- Assets/Scripts/StoreScripts/Inventory.cs
+++ Assets/Scripts/StoreScripts/Inventory.cs
@@ -7,43 +7,55 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour
 {
     //inventory: item index 별 정보를 담기위한 image, sprite, GameObject, price 배열
-    public Image[] itemImages;
-    public Sprite[] itemSprite;
     public GameObject[] itemObj;
     public int[] itemPrice;
 
     public int maxIndex = 0;
     public bool isPull = false;
     
-    //public int AddIndex = 0;
+    public int AddIndex = 0;
 
     //store :item index에 등록된 정보를 inventory의 index에 담기 위한 함수
     public void AddItem(Sprite sprite, string itemprice)
     {
-        if (maxIndex <= 5)
+        if (AddIndex < 6)
         {
-            itemSprite[maxIndex] = sprite;
-            itemObj[maxIndex].gameObject.SetActive(true);
-            itemObj[maxIndex].GetComponent<Image>().sprite = sprite;
-            itemPrice[maxIndex] = int.Parse(itemprice);
-            maxIndex += 1;
+            for (int i = 0; i <= 5; i++)
+            {
+                if (itemObj[i].gameObject.activeSelf == false)
+                {
+                    itemObj[i].gameObject.SetActive(true);
+                    itemObj[i].GetComponent<Image>().sprite = sprite;
+                    itemPrice[i] = int.Parse(itemprice);
+                    AddIndex++;
+                    return;
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
         else
         {
             isPull = true;
-            return;
         }
     }
     //inventory item onclick시 sell 함수
     public void SellItem(int index)
     {
-        if (maxIndex >= 0)
+        if (AddIndex >= 0 && itemObj[index].gameObject.activeSelf == true)
         {
             isPull = false;
+            AddIndex--;
             itemObj[index].gameObject.SetActive(false);
             int price = itemPrice[index];
+            itemPrice[index] = 0;
             Gold.playerGold += price;
-            maxIndex -= 1;
+        }
+        else
+        {
+            return;
         }
     }
 }

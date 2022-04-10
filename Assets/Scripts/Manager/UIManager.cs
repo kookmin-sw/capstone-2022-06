b2c1ede4d5419e242bdfb501f92f7de3cbd9d62f
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-10 19:07:52 +0900

    [변경] UI 최상위 오브젝트, ShowSceneUI, Clear 메서드 추가
    현재 씬의 UI들을 관리하는 최상위 Root 오브젝트를 추가, 게임 씬에서 보여줄 UI를 띄우는 메서드 추가, 현재 씬에 띄워진 UI를 비우는 메서드 추가
    
    관련 이슈 : #40

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -7,9 +7,30 @@ public class UIManager
     // 캔버스 정렬 우선순위
     int _order = 10;
 
-    /*
-    게임 오브젝트에 캔버스를 부착함
-    */
+    UI_Scene _sceneUI = null;
+
+    /// <summary>
+    /// 전체 UI에 상주하는 UI 루트 오브젝트
+    /// </summary>
+    public GameObject Root
+    {
+        get
+        {
+            GameObject root = GameObject.Find("@UI_Root");
+
+            if (!root)
+            {
+                root = new GameObject();
+                root.name = "@UI_Root";
+            }
+
+            return root;
+        }
+    }
+
+    /// <summary>
+    /// 인자로 주어진 게임 오브젝트에 캔버스를 부착합니다.
+    /// </summary>
     public void SetCanvas(GameObject go, bool ordering = true)
     {
         if (go is null)
@@ -79,5 +100,37 @@ public class UIManager
         Canvas canvas = ui.GetOrAddComponent<Canvas>();
         canvas.renderMode = RenderMode.WorldSpace;
         return ui.GetOrAddComponent<T>();
+    }
+
+    /// <summary>
+    /// 매개변수로 받은 이름에 해당하는 UI 프리팹을 게임 씬에 출력할 수 있도록 리턴합니다.
+    /// </summary>
+    public T ShowSceneUI<T>(string name = null) where T : UI_Scene
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            name = typeof(T).Name;
+        }
+
+        GameObject go = Managers.Resource.Instantiate($"UI/Scene/{name}");
+
+        if (!go)
+        {
+            Debug.Log($"Failed to find UI: {name}");
+            return null;
+        }
+
+        T sceneUI = go.GetOrAddComponent<T>();
+        _sceneUI = sceneUI;
+        go.transform.SetParent(Root.transform);
+        return sceneUI;
+    }
+
+    /// <summary>
+    /// 화면에 보이는 모든 UI를 닫습니다. (인게임 UI 제외)
+    /// </summary>
+    public void Clear()
+    {
+        _sceneUI = null;
     }
 }

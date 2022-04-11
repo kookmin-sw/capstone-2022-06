38c6a722c9a869da970804ba4d233344653a9969
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-11 16:33:22 +0900

    [변경] SearchChild()에서 inactive 오브젝트를 가져오도록 변경
    SearchChild() 메서드에서 inactive 오브젝트를 가져오도록 변경 및 summary 추가
    
    관련 이슈 : #40

--- Assets/Scripts/ExLibrary/Util.cs
+++ Assets/Scripts/ExLibrary/Util.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Util
 {
-    /*
-    go의 자식 오브젝트 중 제네릭 T에 해당하는 컴포넌트가 있는지 찾고 없으면 null을 리턴한다.
-    name == null일 경우 오브젝트의 이름은 상관 없이 컴포넌트 타입이 T이기만 하면 먼저 찾은 T를 반환한다.
-    pushdown == true일 경우 go의 모든 후손에 대한 오브젝트에 대해 탐색을 진행한다.
-    */
+    /// <summary>
+    /// go의 자식 오브젝트 중 제네릭 T에 해당하는 컴포넌트가 있는지 찾고 없으면 null을 리턴한다.
+    /// name == null일 경우 오브젝트의 이름은 상관 없이 컴포넌트 타입이 T이기만 하면 먼저 찾은 T를 반환한다.
+    /// pushdown == true일 경우 go의 모든 후손에 대한 오브젝트에 대해 탐색을 진행한다.
+    /// </summary>
     public static T SearchChild<T>(GameObject go, string name = null, bool pushdown = false) where T : UnityEngine.Object
     {
         if (go is null)
@@ -18,7 +18,8 @@ public class Util
 
         if (pushdown)
         {
-            foreach (T e in go.GetComponentsInChildren<T>())
+            // inactive 오브젝트 검색을 허용합니다.
+            foreach (T e in go.GetComponentsInChildren<T>(true))
             {
                 if (string.IsNullOrEmpty(name) || e.name == name)
                 {

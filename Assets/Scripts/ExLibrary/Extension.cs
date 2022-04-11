c34bd4e47a36aceb8c661dc592b7691bd37e3d76
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-12 04:10:33 +0900

    [변경] BindEvent를 Extension에 이관
    BindEvent를 Extension으로 옮김
    
    관련 이슈 : #40

--- Assets/Scripts/ExLibrary/Extension.cs
+++ Assets/Scripts/ExLibrary/Extension.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public static class Extension
 {
@@ -13,5 +15,30 @@ public static class Extension
             return ret;
         }
         return go.AddComponent<T>();
+    }
+
+    /// <summary>
+    /// GameObject go에게 UI_EventHandler와 action을 부착함
+    /// eventType의 기본값은 Click
+    /// </summary>
+    public static void BindEvent(this GameObject go, Action<PointerEventData> action, Define.UIEvent eventType = Define.UIEvent.Click)
+    {
+        UI_EventHandler evt = go.GetOrAddComponent<UI_EventHandler>();
+
+        switch (eventType)
+        {
+            case Define.UIEvent.Click:
+            {
+                evt.OnClickHandler -= action;
+                evt.OnClickHandler += action;
+                break;
+            }
+            case Define.UIEvent.Drag:
+            {
+                evt.OnDragHandler -= action;
+                evt.OnClickHandler += action;
+                break;
+            }
+        }
     }
 }

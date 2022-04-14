e2aa37df105d09f097abe419dcbda58e43269aa4
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-14 23:00:22 +0900

    [추가] 게임 준비 화면 UI 클래스 추가
    게임 준비 화면 UI를 관리하는 클래스 추가
    
    관련 이슈 : #46

--- Assets/Scripts/UI/Scene/UI_Preparation.cs
+++ Assets/Scripts/UI/Scene/UI_Preparation.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class UI_Preparation : UI_Scene
 {
-    
+    private GameObject contentsDiv = null;
+
+    enum GameObjects
+    {
+        SelectedView,
+        PickUpBoard,
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+        Bind<GameObject>(typeof(GameObjects));
+
+        contentsDiv = Util.SearchChild(
+            Util.SearchChild(Get<GameObject>((int)GameObjects.PickUpBoard), "Viewport"),
+            "ContentsDiv"
+        );
+
+        // May delete place holder ui objects but not delete yet.
+        // foreach (Transform child in contentsDiv.transform)
+        // {
+        //     Managers.Resource.Destroy(child.gameObject);
+        // }
+
+
+        // Managers.UI.AttachSubItem
+    }
 }

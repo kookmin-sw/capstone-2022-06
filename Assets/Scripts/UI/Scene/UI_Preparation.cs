4de6648269d329d081904f38e09ab343ff0cddc6
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-23 04:22:08 +0900

    [변경] 게임 준비 씬 로드시 json 파일을 파싱하도록 함
    게임 준비 씬에 들어갈 때 HeroIcon.json 파일을 읽어 초상화 UI를 추가하는 코드 추가
    
    관련 이슈 : #46

--- Assets/Scripts/UI/Scene/UI_Preparation.cs
+++ Assets/Scripts/UI/Scene/UI_Preparation.cs
@@ -1,6 +1,20 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+class Icons
+{
+    public List<HeroInfo> Contents;
+}
+
+[System.Serializable]
+class HeroInfo 
+{
+    public string path;
+}
 
 public class UI_Preparation : UI_Scene
 {
@@ -23,12 +37,35 @@ public class UI_Preparation : UI_Scene
             "ContentsDiv"
         );
 
-        // May delete place holder ui objects but not delete yet.
-        // foreach (Transform child in contentsDiv.transform)
-        // {
-        //     Managers.Resource.Destroy(child.gameObject);
-        // }
+        // Delete dummy icons
+        foreach (Transform child in contentsDiv.transform)
+        {
+            Managers.Resource.Destroy(child.gameObject);
+        }
 
+        FileInfo jsonInfo = new FileInfo("Assets/Scripts/JSON/HeroIcons.json");
+
+        if (jsonInfo.Exists is not false)
+        {
+            string jsonString = File.ReadAllText(jsonInfo.FullName);
+            Icons loadedIcons = JsonUtility.FromJson<Icons>(jsonString);
+
+            for (int i = 0; i < loadedIcons.Contents.Count; i++)
+            {
+                HeroInfo info = loadedIcons.Contents[i];
+                Sprite heroPortrait = Managers.Resource.Load<Sprite>(info.path);
+                UI_Portrait portrait = Managers.UI.AttachSubItem<UI_Portrait>(contentsDiv.transform);
+                portrait.GetComponent<Image>().sprite = heroPortrait;
+                portrait.name = $"Portrait_{i + 1}";
+
+                // int idx = portrait.name.IndexOf("(Clone)");
+                // portrait.name = portrait.name.Replace(portrait.name.Substring(idx, 7), (i + 1).ToString());
+            }
+        }
+        else
+        {
+            Debug.Log($"Failed to load json file {jsonInfo.Name}");
+        }
 
         // Managers.UI.AttachSubItem
     }

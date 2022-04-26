7e3a851f8182d2e649a6cb85acf430593f98b1f3
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-27 04:04:03 +0900

    [변경] PrepareRoundManager 일부 코드를 옮김
    싱글톤 관리를 위해 RoundManager에 있던 코드 일부를 UI_Preparation으로 이주
    
    관련 이슈 : #46

--- Assets/Scripts/UI/Scene/UI_Preparation.cs
+++ Assets/Scripts/UI/Scene/UI_Preparation.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Photon.Pun;
+using Photon.Realtime;
+using HashTable = ExitGames.Client.Photon.Hashtable;
 
 [System.Serializable]
 class Icons
@@ -18,7 +22,16 @@ class HeroInfo
 
 public class UI_Preparation : UI_Scene
 {
+    private PhotonView PV;
+
     private GameObject contentsDiv = null;
+
+    private string iconJsonPath = "Assets/Scripts/JSON/HeroIcons.json";
+    private int myLocalId = -1;
+    private int preparedCount = 0;
+    private string selectedIcon = null;
+    
+    public Hashtable _playerCustomProperties = new Hashtable();
 
     enum GameObjects
     {
@@ -37,15 +50,19 @@ public class UI_Preparation : UI_Scene
             "ContentsDiv"
         );
 
+        PV = GetComponent<PhotonView>();
+        myLocalId = GetLocalId();
+
         // Delete dummy icons
         foreach (Transform child in contentsDiv.transform)
         {
             Managers.Resource.Destroy(child.gameObject);
         }
 
-        FileInfo jsonInfo = new FileInfo("Assets/Scripts/JSON/HeroIcons.json");
+        FileInfo jsonInfo = new FileInfo(iconJsonPath);
 
-        if (jsonInfo.Exists is not false)
+        // 찾는 json 파일이 존재하는 경우 파싱해서 초상화를 불러옵니다.
+        if (jsonInfo.Exists)
         {
             string jsonString = File.ReadAllText(jsonInfo.FullName);
             Icons loadedIcons = JsonUtility.FromJson<Icons>(jsonString);
@@ -54,12 +71,14 @@ public class UI_Preparation : UI_Scene
             {
                 HeroInfo info = loadedIcons.Contents[i];
                 Sprite heroPortrait = Managers.Resource.Load<Sprite>(info.path);
-                UI_Portrait portrait = Managers.UI.AttachSubItem<UI_Portrait>(contentsDiv.transform);
-                portrait.GetComponent<Image>().sprite = heroPortrait;
-                portrait.name = $"Portrait_{i + 1}";
+                UI_PortraitButton portrait = Managers.UI.AttachSubItem<UI_PortraitButton>(contentsDiv.transform);
 
-                // int idx = portrait.name.IndexOf("(Clone)");
-                // portrait.name = portrait.name.Replace(portrait.name.Substring(idx, 7), (i + 1).ToString());
+                portrait.GetComponent<Image>().sprite = heroPortrait;
+                portrait.name = $"Portrait{i + 1}";
+                portrait.GetComponent<Button>().onClick.AddListener(() => {
+                    GameObject selected = EventSystem.current.currentSelectedGameObject;
+                    selectedIcon = selected.GetComponent<Sprite>().name;
+                });
             }
         }
         else
@@ -68,5 +87,36 @@ public class UI_Preparation : UI_Scene
         }
 
         // Managers.UI.AttachSubItem
+    }
+
+    /// <summary>
+    /// 자신이 만들어진 room에서 몇번째로 들어왔는지 찾습니다.
+    /// 자신의 아이디를 찾지 못하면 -1을 반환합니다.
+    /// </summray>
+    private int GetLocalId()
+    {
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            Player p = PhotonNetwork.PlayerList[i];
+            if (p.UserId == PhotonNetwork.LocalPlayer.UserId)
+            {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    /// <summary>
+    /// 모든 플레이어가 영웅을 선택할지 기다리는 코루틴입니다.
+    /// </summary>
+    private IEnumerator WaitAllReady()
+    {
+        yield return new WaitUntil(() => {
+            return preparedCount == PhotonNetwork.CurrentRoom.PlayerCount;
+        });
+
+        Debug.Log($"All player get ready : {preparedCount}");
+        PhotonNetwork.LoadLevel("GameScene");
     }
 }

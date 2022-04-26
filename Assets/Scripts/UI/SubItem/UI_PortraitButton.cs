01d0cba7c1ba029658a5954308ea0331dfacc546
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-27 03:33:17 +0900

    [변경] UI_Portrait를 UI_PortraitButton으로 변경
    좀 더 알아보기 수월하도록 이름을 변경
    
    관련 이슈 : #46

--- Assets/Scripts/UI/SubItem/UI_PortraitButton.cs
+++ Assets/Scripts/UI/SubItem/UI_PortraitButton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_Portrait : UI_Base
+public class UI_PortraitButton : UI_Base
 {
     public override void Init()
     {

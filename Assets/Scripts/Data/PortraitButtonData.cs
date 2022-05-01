1940567814220aaf0799baf0a75e1af533b3afa7
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-01 18:21:45 +0900

    [리팩토링] PortraitButtonData에 주석 부착
    PortraitButtonData에 주석을 부착
    
    관련 이슈 : #46

--- Assets/Scripts/Data/PortraitButtonData.cs
+++ Assets/Scripts/Data/PortraitButtonData.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// PreparationScene에서 선택한 영웅의 게임 진행에 필요한 정보를 저장하는 클래스 입니다.
+/// </summary>
 public class PortraitButtonData : MonoBehaviour
 {
     private string prefabPath, heroName;

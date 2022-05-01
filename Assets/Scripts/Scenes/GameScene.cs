649b7879cc155b636aa4af12a55d08033ba6cd9e
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-01 16:20:57 +0900

    [변경] 해쉬 테스트 코드 제거
    테스트가 완료된 테스트 코드를 제거
    
    관련 이슈 : #46

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -2,15 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using PhotonHash = ExitGames.Client.Photon.Hashtable;
 
 public class GameScene : BaseScene
 {
-    PhotonHash myHash;
-
     void Start()
     {
-        Debug.Log(PhotonNetwork.LocalPlayer.CustomProperties["PlayerReady"]);
     }
 
     public override void Clear() {}

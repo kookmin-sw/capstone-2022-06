10141fb04865b4b1e9135aeec8c5af841db979ac
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-01 23:51:00 +0900

    [변경] 프로퍼티 디버그 코드 추가
    CustomProperties가 무사히 보존되었는지 확인하는 코드 추가
    
    관련 이슈 : #46

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -5,9 +5,22 @@ using Photon.Pun;
 
 public class GameScene : BaseScene
 {
+    int isCommander;
+    int myId;
+
     void Start()
     {
+        isCommander = (int)PhotonNetwork.LocalPlayer.CustomProperties["isCommander"];
+        myId = (int)PhotonNetwork.LocalPlayer.CustomProperties["matchId"];
+
+        // PritMyProp();
     }
 
     public override void Clear() {}
+
+    private void PrintMyProp()
+    {
+        Debug.Log($"Status of commander {isCommander}");
+        Debug.Log($"Status of local id {myId}");
+    }
 }

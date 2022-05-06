a836d66471b3ea6efff80411728c2da51e989996
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-06 17:13:17 +0900

    [변경] GameScene 로드시 룸 프로퍼티에 타임스탬프 저장
    GameScene 최초 로드할시 마스터 클라이언트에 한하여 startTimestamp 프로퍼티를 넣도록 변경
    
    관련 이슈 : #68

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using photonHash = ExitGames.Client.Photon.Hashtable;
 
 public class GameScene : BaseScene
 {
@@ -22,5 +23,19 @@ public class GameScene : BaseScene
     {
         Debug.Log($"Status of commander {isCommander}");
         Debug.Log($"Status of local id {myId}");
+    }
+
+    protected override void Init()
+    {
+        base.Init();
+
+        // 게임 시작 시간을 커스텀 프로퍼티로 저장합니다.
+        // 마스터만 저장하도록 강제합니다.
+        if (PhotonNetwork.IsMasterClient)
+        {
+            photonHash roomHash = PhotonNetwork.CurrentRoom.CustomProperties;
+            roomHash.Add("startTimestamp", Time.time);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(roomHash);
+        }
     }
 }

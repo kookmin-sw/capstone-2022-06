7bd358b416b645b93a19bf2070bffc09a8cf1243
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-07 03:22:59 +0900

    [변경] Text bind, 플레이 타임을 바꾸는 코드 추가
    Update마다 현재 시간을 불러와 포톤 룸 프로퍼티로 저장한 값으로 게임 경과 시간을 계산 후 TimeText를 변경하는 코드 작성
    
    관련 이슈 : #68

--- Assets/Scripts/UI/Scene/UI_GameScene.cs
+++ Assets/Scripts/UI/Scene/UI_GameScene.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class UI_GameScene : UI_Scene
 {
+    enum Texts
+    {
+        TimeText
+    }
+
+    void Update()
+    {
+        object stamp;
+        if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("startTimestamp", out stamp))
+        {
+            float elapseTime = Time.time - (float)stamp;
+            GetText((int)Texts.TimeText).text = elapseTime.ToString();
+        }
+    }
+
     public override void Init()
     {
         base.Init();
+
+        Bind<Text>(typeof(Texts));
     }
 }

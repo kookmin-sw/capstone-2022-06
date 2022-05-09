bdc8e921cf3acc1a81d265fb14ceee22f8ca3123
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-10 02:39:56 +0900

    [변경] 지휘관 카메라 이동 로직 수정
    스크린 좌표와 마우스 위치를 대조하여 커서가 화면 끝에 다다를 때 성분 값으로 1 또는 -1을 할당하고 Translate로 이동하도록 함. 카메라의 위치가 경계에 다다르면 경계 안으로 고정시킴
    
    관련 이슈 : #76

--- Assets/Scripts/Commander/CommanderCamController.cs
+++ Assets/Scripts/Commander/CommanderCamController.cs
@@ -4,17 +4,70 @@ using UnityEngine;
 
 public class CommanderCamController : MonoBehaviour
 {
-    public float scrollSpeed = 15f;
+    public float scrollSpeed = 50f;
+    Camera myCam;
+
+    void Start()
+    {
+        myCam = GetComponent<Camera>();
+    }
 
     void Update()
     {
         float mouseY = Input.mousePosition.y;
         float mouseX = Input.mousePosition.x;
-        if (mouseY <= Screen.height * 0.05 || mouseY >= Screen.height * 0.95 ||
-        mouseX <= Screen.width * 0.05 || mouseX >= Screen.width * 0.95)
+
+        Vector3 dir = new Vector3(0, 0, 0);
+
+        if (mouseX <= Screen.width * 0.05)
         {
-            Vector3 dir = new Vector3(mouseX, Camera.main.transform.position.y, mouseY);
-            Camera.main.transform.Translate(dir * Time.deltaTime * scrollSpeed, Space.World);
+            dir.x = -1;
+        }
+
+        if (mouseX >= Screen.width * 0.95)
+        {
+            dir.x = 1;
+        }
+
+        if (mouseY >= Screen.height * 0.95)
+        {
+            dir.z = 1;
+        }
+
+        if (mouseY <= Screen.height * 0.05)
+        {
+            dir.z = -1;
+        }
+
+        myCam.transform.Translate(dir * Time.deltaTime * scrollSpeed, Space.World);
+        Vector3 pos = myCam.transform.position;
+        bool bound = false;
+
+        if (myCam.transform.position.x < -105)
+        {
+            bound = true;
+            pos.x = -105;
+        }
+        else if (myCam.transform.position.x > 105)
+        {
+            bound = true;
+            pos.x = 105;
+        }
+
+        if (myCam.transform.position.z < -105)
+        {
+            bound = true;
+            pos.z = -105;
+        }
+        else if (myCam.transform.position.z > 105)
+        {
+            bound = true;
+            pos.z = 105;
+        }
+
+        if (bound)
+        {
+            myCam.transform.position = pos;
         }
     }
 }

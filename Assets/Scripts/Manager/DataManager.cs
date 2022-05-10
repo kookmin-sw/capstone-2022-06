00ddff0309b0d332d0f118323fe98c58333fc7de
namgyeongtae <nrudxo12@kookmin.ac.kr>
2022-05-10 22:36:18 +0900

    Update DataManager.cs
    Add Monster Stat Dictionary to DataManager
    
    see also : #80

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -13,12 +13,14 @@ public class DataManager
     public Dictionary<string, Stat> StatDict { get; private set; } = new Dictionary<string, Stat>();    // √®««æ Ω∫≈»¿ª ¿˙¿Â«— µÒº≈≥ ∏Æ
     public Dictionary<string, Stat> MinionStatDict { get; private set; } = new Dictionary<string, Stat>();
     public Dictionary<string, Stat> TurretStatDict { get; private set; } = new Dictionary<string, Stat>();
+    public Dictionary<string, Stat> MonsterStatDict { get; private set; } = new Dictionary<string, Stat>();
 
     public void Init()
     {
         StatDict = LoadJson<StatData, string, Stat>("StatData").MakeDict();
         MinionStatDict = LoadJson<StatData, string, Stat>("MinionStatData").MakeDict();
         TurretStatDict = LoadJson<StatData, string, Stat>("TurretStatData").MakeDict();
+        MonsterStatDict = LoadJson<StatData, string, Stat>("MonsterStatData").MakeDict();
     }
 
     Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>

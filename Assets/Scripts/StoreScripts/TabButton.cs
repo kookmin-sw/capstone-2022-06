779fb8e3786dc6c1a277f6f3a22e22b1144fefb1
Suyeon Lee <suyeon9477@gmail.com>
2022-05-10 23:15:02 +0900

    [변경] TabButton 내용 수정

--- Assets/Scripts/StoreScripts/TabButton.cs
+++ Assets/Scripts/StoreScripts/TabButton.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class TabButton : MonoBehaviour
+public class TabButton : MonoBehaviour, IPointerClickHandler
 {
     // item 카테고리 select 시 배경 변경 -> 아닐시 기본 배경으로 변경
+    ShopManager SM;
+
     Image background;
     public Sprite idleImg;
     public Sprite selectedImg;
     private void Awake()
     {
         background = GetComponent<Image>();
+        SM = GameObject.Find("ShopManager").GetComponent<ShopManager>();
     }
     public void Selected()
     {
@@ -20,5 +24,12 @@ public class TabButton : MonoBehaviour
     public void DeSelected()
     {
         background.sprite = idleImg;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            SM.TabSelect(this);
+        }
     }
 }

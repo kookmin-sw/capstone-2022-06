69098191d5b22130606727b96b764e126e8111b9
Suyeon Lee <suyeon9477@gmail.com>
2022-05-10 23:18:29 +0900

    [추가] 인벤토리 관리를 위한 InventorySlotRecord Script 추가

---
+++ Assets/Scripts/StoreScripts/InventorySlotRecord.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlotRecord
+{
+    public Item item;
+    public bool iswardslot = false;
+    public bool occupied = false;
+    public int stackednum = 0;
+}

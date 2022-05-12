ff4e4913b8c7a810aa73688ad976e7d731f2f8e3
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-13 03:17:32 +0900

    [변경] SetLayer에 OffRenderer를 호출하는 코드 추가
    SetLayer를 호출할 때 안 보이게 하는 기능을 쉽게 구현하기 위해 OffRenderer를 추가하도록 함
    
    관련 이슈 : #77

--- Assets/Scripts/Character/LayerController.cs
+++ Assets/Scripts/Character/LayerController.cs
@@ -12,9 +12,19 @@ public class LayerController : MonoBehaviour
         PV = GetComponent<PhotonView>();
     }
 
+    /// </summary>
+    /// 레이어를 설정합니다.
+    /// 겸사겸사 자기 팀 레이어가 아니면 OffRenderer를 호출합니다.
+    /// </summary>
     public void SetLayer(string layerName)
     {
         PV.RPC("_SetLayer", RpcTarget.All, layerName);
+        int id = (int)PhotonNetwork.LocalPlayer.CustomProperties["actorId"];
+        if ((id <= 5 && gameObject.layer == LayerMask.NameToLayer("RedTeam")) ||
+        (id > 5 && gameObject.layer == LayerMask.NameToLayer("BlueTeam")))
+        {
+            Util.OffRenderer(transform);
+        }
     }
 
     [PunRPC]

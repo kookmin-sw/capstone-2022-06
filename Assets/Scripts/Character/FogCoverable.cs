b9eda8ce1420d28f3bd9dbf56a4c80b4c9b44188
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-12 16:18:43 +0900

    [변경] Contains가 false일 때 OffRenderer 호출
    newTargets에서 transform이 없으면 OffRenderer를 호출하도록 변경
    
    관련 이슈 : #77

--- Assets/Scripts/Character/FogCoverable.cs
+++ Assets/Scripts/Character/FogCoverable.cs
@@ -15,5 +15,9 @@ public class FogCoverable : MonoBehaviour
         {
             Util.OnRenderer(gameObject);
         }
+        else
+        {
+            Util.OffRenderer(gameObject);
+        }
     }
 }

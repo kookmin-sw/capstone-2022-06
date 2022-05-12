2816b00d67596d418abc48c49673ac13685c10a4
namgyeongtae <nrudxo12@kookmin.ac.kr>
2022-05-12 17:26:05 +0900

    Update MonsterController.cs
    Update Trace Function
    
    see also : #80

--- Assets/Scripts/Monster/MonsterController.cs
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -15,6 +15,9 @@ public class MonsterController : Controller
     public float _traceSpeed;
     public float _attackRange;
 
+    public float _traceTime;
+    public float _traceLimitTime;
+
     public enum MonsterState
     {
         IDLE,
@@ -24,10 +27,10 @@ public class MonsterController : Controller
         DIE
     }
 
-    MonsterState _state;
+    public MonsterState _state;
 
-    public Transform[] wayPoints;
-    GameObject _lockTarget;
+    public List<Transform> wayPoints = new List<Transform>();
+    public GameObject _lockTarget;
     NavMeshAgent _nav;
     Animator MonsterAnimator;
 
@@ -39,11 +42,15 @@ public class MonsterController : Controller
         _state = MonsterState.WALK;
         _stat = GetComponent<MonsterStat>();
         _stat.Initialize("CrabMonster");
-        targetLayer = 1 << LayerMask.NameToLayer("BlueTeam") | LayerMask.NameToLayer("RedTeam");
+        _nav = GetComponent<NavMeshAgent>();
+        MonsterAnimator = GetComponent<Animator>();
+        
+        targetLayer = 1 << LayerMask.NameToLayer("RedTeam");
+
+        InitWayPoints();
 
         _nav.SetDestination(wayPoints[destNum].position);
 
-        MonsterAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -63,6 +70,9 @@ public class MonsterController : Controller
                 UpdateAttack();
                 break;
         }
+
+        if (Input.GetKeyDown(KeyCode.W))
+            TakeDamage(1);
     }
 
     void UpdateWalk()
@@ -70,19 +80,40 @@ public class MonsterController : Controller
         // WALK 애니메이션 재생
         MonsterAnimator.SetBool("IsTrace", false);
         // 배회 알고리즘 (wayPoints 에 있는 transform들을 순서대로 목표하여 배회하도록)
+        _nav.speed = _walkSpeed;
         ChangeWayPoint();
+
+        _nav.SetDestination(wayPoints[destNum].position);
         // 조건에 따른 _state 변경
     }
 
     void UpdateTrace()
     {
+        _traceTime += Time.deltaTime;
         // RUN 애니메이션 재생
+        MonsterAnimator.SetBool("IsTrace", true);
+        MonsterAnimator.SetBool("IsAttack", false);
         // 타겟을 SetDestination하여 추적
+        _nav.speed = _traceSpeed;
+        _nav.SetDestination(_lockTarget.transform.position);
 
-        // 타겟과의 거리가 공격범위 내에 들어오면 _state 를 ATTACK으로 변경
-        // 타겟과의 거리가 일정 범위 벗어나면 target = null, _state도 WALK로 변경
+        // 타겟과의 거리가 공격범위 내에 들어오면 _state 를 ATTACK으로 변경 _traceTime = 0;
+        // 타겟과의 거리가 일정 범위 벗어나면 target = null, _state도 WALK로 변경 _traceTime = 0;
+        if (Vector3.Distance(transform.position, _lockTarget.transform.position) >= 50.0f)
+        {
+            _lockTarget = null;
+            _traceTime = 0;
+            _state = MonsterState.WALK;
+            return;
+        }
 
         // TRACE 상태를 일정시간동안 유지만 하면 _state를 WALK로 변경하고 target = null 
+        if (_traceTime >= _traceLimitTime)
+        {
+            _lockTarget = null;
+            _traceTime = 0;
+            _state = MonsterState.WALK;
+        }
     }
 
     void UpdateAttack()
@@ -103,7 +134,7 @@ public class MonsterController : Controller
         // 피격 시 타겟 Lock
         if (_state == MonsterState.WALK)
         {
-            Collider[] targetCandidates = Physics.OverlapSphere(transform.position, 7.0f, targetLayer);
+            Collider[] targetCandidates = Physics.OverlapSphere(transform.position, 20.0f, targetLayer);
 
             Array.Sort(targetCandidates, delegate (Collider a, Collider b)
             {
@@ -131,9 +162,28 @@ public class MonsterController : Controller
         }
     }
 
+    // 애니메이션 이벤트
     public void OnHit()
     {
+        ObjectStat targetStat = _lockTarget.GetComponent<ObjectStat>();
+
         // target에게 데미지를 입힘
+        if (_lockTarget.GetComponent<Controller>() != null)
+            _lockTarget.GetComponent<Controller>().TakeDamage(_stat.Status.atk);
+
+        if (targetStat.Status.hp <= 0)
+        {
+            _lockTarget = null;
+            _state = MonsterState.WALK;
+        }
+    }
+
+    public void InitWayPoints()
+    {
+        Transform tf = GameObject.Find("MonsterWayPoints").transform;
+
+        for (int i = 0; i < tf.childCount; i++)
+            wayPoints.Add(tf.GetChild(i));
     }
 
     public void ChangeWayPoint()
@@ -142,9 +192,7 @@ public class MonsterController : Controller
         {
             ++destNum;
 
-            if (destNum >= wayPoints.Length) destNum = 0;
-
-            _nav.SetDestination(wayPoints[destNum].position);
+            if (destNum >= wayPoints.Count) destNum = 0;
         }
     }
 }

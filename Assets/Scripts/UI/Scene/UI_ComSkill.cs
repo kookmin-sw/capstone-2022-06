7c150cce113efe5f1e827a4aa44d2e89f509a25e
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-15 02:35:32 +0900

    [추가] UI_ComSkill 클래스 추가
    UI_ComSkill UI 오브젝트를 관리할 스크립트 추가
    
    관련 이슈 : #78

---
+++ Assets/Scripts/UI/Scene/UI_ComSkill.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ComSkill : UI_Base
+{
+    public override void Init()
+    {
+        
+    }
+}

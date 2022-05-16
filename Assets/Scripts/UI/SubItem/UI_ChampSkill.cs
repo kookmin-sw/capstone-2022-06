181df4ec020f79b694cd314d1dd986a2f5c52f0b
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-16 20:38:07 +0900

    [변경] 초기 스킬 오브젝트 바인드 및 키 입력 감지 메서드 추가
    초기 스킬 오브젝트 바인드 및 키 입력 감지 메서드 추가
    
    관련 이슈 : #103

--- Assets/Scripts/UI/SubItem/UI_ChampSkill.cs
+++ Assets/Scripts/UI/SubItem/UI_ChampSkill.cs
@@ -6,5 +6,29 @@ public class UI_ChampSkill : UI_Base
 {
     public override void Init()
     {
+        Bind<GameObject>(typeof(GameObjects));
+    }
+
+    void Update()
+    {
+        OnKeyPressed();
+    }
+
+    void OnKeyPressed()
+    {
+        if (Input.GetKey("q"))
+        {
+
+        }
+        else if (Input.GetKey("w"))
+        {
+
+        }
+    }
+
+    enum GameObjects
+    {
+        Meteor,
+        Heal
     }
 }

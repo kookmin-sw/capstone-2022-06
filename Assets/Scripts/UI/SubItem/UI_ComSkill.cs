c0e90250493910b42f373c0f5ec897a3539ba99f
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-17 00:34:59 +0900

    [변경] 키코드 딕셔너리 생성
    키코드와 마스크 인덱스를 매칭하는 딕셔너리 생성
    
    관련 이슈 : #103

--- Assets/Scripts/UI/SubItem/UI_ComSkill.cs
+++ Assets/Scripts/UI/SubItem/UI_ComSkill.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class UI_ComSkill : UI_Base
 {
     UI_AreaIndicator skillPointer;
+    int skillMask = 0;
     int skillType;
+    int skillAble = 0;
     string[] skillPaths = {
         "Arts/Particles/Meteor",
         "Arts/Particles/LeavesBuff"
     };
+    UnityEngine.KeyCode[] keyIndex = {
+        KeyCode.Q,
+        KeyCode.W
+    };
+
+    Dictionary<UnityEngine.KeyCode, int> keyIndexDict = new Dictionary<KeyCode, int>();
 
     public override void Init()
     {
         Bind<GameObject>(typeof(GameObjects));
         skillPointer = Managers.UI.AttachWorldUI<UI_AreaIndicator>();
         skillPointer.gameObject.SetActive(false);
+
+        for (int i = 0; i < keyIndex.Length; i++)
+        {
+            keyIndexDict[keyIndex[i]] = i + 1;
+        }
     }
 
     void Update()
@@ -28,30 +41,38 @@ public class UI_ComSkill : UI_Base
     {
         skillType = -1;
 
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKeyUp(KeyCode.Q))
         {
             if (!Get<GameObject>((int)GameObjects.Meteor).GetComponent<UI_Cooldown>().IsCooldownFinished())
             {
                 return;
             }
 
+            skillAble ^= 1;
             skillType = 0;
         }
-        else if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKeyUp(KeyCode.W))
         {
             if (!Get<GameObject>((int)GameObjects.Heal).GetComponent<UI_Cooldown>().IsCooldownFinished())
             {
                 return;
             }
 
+            skillAble ^= 1;
             skillType = 1;
         }
-
-        // 스킬 사용가능, skillPointer 활성화
-        if (skillType > -1)
+        else if (Input.GetKeyUp(KeyCode.Escape))
         {
-            skillPointer.gameObject.SetActive(true);
+            skillAble = 0;
         }
+
+        // skillAble에 따라 skillPointer 활성화 혹은 비활성화
+        skillPointer.gameObject.SetActive(skillAble == 1);
+    }
+
+    void PropagateSkillState(bool able)
+    {
+
     }
 
     enum GameObjects

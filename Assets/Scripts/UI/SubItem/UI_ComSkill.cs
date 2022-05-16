e6860812abca060357f634c07e7eac2baa87b93a
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-17 00:54:50 +0900

    [리팩토링] 불필요 코드 삭제 및 코드 길이 감소
    불필요 코드를 삭제하고 삼항 연산자를 사용해 코드 길이 감소
    
    관련 이슈 : #103

--- Assets/Scripts/UI/SubItem/UI_ComSkill.cs
+++ Assets/Scripts/UI/SubItem/UI_ComSkill.cs
@@ -6,29 +6,17 @@ public class UI_ComSkill : UI_Base
 {
     UI_AreaIndicator skillPointer;
     int skillMask = 0;
-    int skillType;
     bool skillAble = false;
     string[] skillPaths = {
         "Arts/Particles/Meteor",
         "Arts/Particles/LeavesBuff"
     };
-    UnityEngine.KeyCode[] keyIndex = {
-        KeyCode.Q,
-        KeyCode.W
-    };
-
-    Dictionary<UnityEngine.KeyCode, int> keyIndexDict = new Dictionary<KeyCode, int>();
 
     public override void Init()
     {
         Bind<GameObject>(typeof(GameObjects));
         skillPointer = Managers.UI.AttachWorldUI<UI_AreaIndicator>();
         skillPointer.gameObject.SetActive(false);
-
-        for (int i = 0; i < keyIndex.Length; i++)
-        {
-            keyIndexDict[keyIndex[i]] = i + 1;
-        }
     }
 
     void Update()
@@ -39,9 +27,7 @@ public class UI_ComSkill : UI_Base
     /// <summary> 지휘관 스킬 키를 눌렀을 때 일어나는 동작을 작성한 메서드 </summary>
     void OnKeyPressed()
     {
-        skillType = -1;
-
-        // escape 감지시 스킬 비활성화는 게임 강제 종료 이슈를 고려하여 보류
+        // escape 감지시 스킬 비활성화는 게임 종료 이슈를 고려하여 보류
         // if (Input.GetKeyUp(KeyCode.Escape))
         // {
         //     skillPointer.gameObject.SetActive(skillAble);
@@ -56,15 +42,8 @@ public class UI_ComSkill : UI_Base
             }
 
             skillMask ^= (1 << 0);
-            if (skillMask > 0)
-            {
-                skillAble = true;
-                skillMask = 1 << 0;
-            }
-            else
-            {
-                skillAble = false;
-            }
+            skillAble = skillMask > 0;
+            skillMask = skillAble ? 1 << 0 : 0;
         }
         else if (Input.GetKeyUp(KeyCode.W))
         {
@@ -74,24 +53,28 @@ public class UI_ComSkill : UI_Base
             }
 
             skillMask ^= (1 << 1);
-            if (skillMask > 0)
-            {
-                skillAble = true;
-                skillMask = 1 << 1;
-            }
-            else
-            {
-                skillAble = false;
-            }
+            skillAble = skillMask > 0;
+            skillMask = skillAble ? 1 << 1 : 0;
         }
 
         // skillAble에 따라 skillPointer 활성화 혹은 비활성화
         skillPointer.gameObject.SetActive(skillAble);
     }
 
-    void PropagateSkillState(bool able)
+    /// <summary>
+    /// 현재 활성화된 스킬을 찾습니다. 없으면 null을 리턴합니다.
+    /// </summary>
+    string GetCurrentSkillPath()
     {
+        for (int i = 0; i < skillPaths.Length; i++)
+        {
+            if ((skillMask & (1 << i)) > 0)
+            {
+                return skillPaths[i];
+            }
+        }
 
+        return null;
     }
 
     enum GameObjects

f7c472189148754ad349d5277d03bb33ad7bcc1b
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-17 19:43:09 +0900

    [변경] Controller를 가진 적에게 데미지를 부여하는 코드 추가
    지휘관의 공격 스킬이 Controller를 가진 적에게 데미지를 부여하는 코드 추가
    
    관련 이슈 : #117

--- Assets/Scripts/Particle/Meteor.cs
+++ Assets/Scripts/Particle/Meteor.cs
@@ -8,8 +8,39 @@ public class Meteor : ParticleBase
     void Start()
     {
         base.Init();
+        Invoke("GiveDamage", 0.5f);
+    }
 
-        // 실험을 위한 코드
-        Physics.OverlapSphere(transform.position, 7f, LayerMask.GetMask("BlueTeam"));
+    /// <summary>
+    /// 반경 내 적에게 데미지를 입히는 함수입니다.
+    /// 아직 미니언에게만 데미지를 입힙니다.
+    /// </summary>
+    void GiveDamage()
+    {
+        int enemyLayer = Util.GetEnemyLayer();
+        Collider[] candidates = Physics.OverlapSphere(transform.position, 7f, 1 << enemyLayer);
+
+        for (int i = 0; i < candidates.Length; i++)
+        {
+            RaycastHit hit;
+            Vector3 dir = (candidates[i].transform.position - transform.position);
+            float dist = dir.magnitude;
+
+            // hit 한 경우 무시
+            if (Physics.Raycast(transform.position, dir.normalized, out hit, dist, LayerMask.GetMask("Obstacle")))
+            {
+                continue;
+            }
+
+            GameObject target = candidates[i].gameObject;
+            Controller targetStat = target.GetComponent<Controller>();
+
+            if (!targetStat)
+            {
+                continue;
+            }
+
+            targetStat.TakeDamage(200f);
+        }
     }
 }

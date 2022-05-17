815a021e82edef6bd8708e01b9cb138bb7285eec
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-17 19:51:17 +0900

    [변경] ScanFriendlies 추가
    같은 팀 유닛들을 검색하는 메서드 추가
    
    관련 이슈 : #117

--- Assets/Scripts/Particle/ParticleBase.cs
+++ Assets/Scripts/Particle/ParticleBase.cs
@@ -30,4 +30,11 @@ public class ParticleBase : MonoBehaviour
         int enemyLayer = Util.GetEnemyLayer();
         return Physics.OverlapSphere(transform.position, radius, 1 << enemyLayer);
     }
+
+    /// <summary> 아군 유닛들을 overlapSphere로 스캔하는 메서드 </summary>
+    protected Collider[] ScanFriendlies(float radius)
+    {
+        int friendLayer = Util.GetEnemyLayer();
+        return Physics.OverlapSphere(transform.position, radius, 1 << friendLayer);
+    }
 }

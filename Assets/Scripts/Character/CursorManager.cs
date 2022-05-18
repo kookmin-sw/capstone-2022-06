e3b950d7666253648543d55fb2afd58eecc3a3b1
namgyeongtae <nrudxo12@kookmin.ac.kr>
2022-05-18 12:16:23 +0900

    Create CursorManager
    Create cursor script
    
    see also : #118

---
+++ Assets/Scripts/Character/CursorManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    int enemyLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (this.gameObject.layer == LayerMask.NameToLayer("RedTeam"))
+            enemyLayer = LayerMask.NameToLayer("BlueTeam");
+        else
+            enemyLayer = LayerMask.NameToLayer("RedTeam");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMouseCursor();
+    }
+
+    void UpdateMouseCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit[] hits;
+
+        hits = Physics.RaycastAll(ray);
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.gameObject.layer == enemyLayer && hit.collider == hit.collider.gameObject.GetComponent<CapsuleCollider>())
+            {
+                Texture2D tex = Managers.Resource.Load<Texture2D>("Textures/Cursors/Cursor_Attack");
+                Cursor.SetCursor(tex, new Vector2(tex.width / 5, 0), CursorMode.Auto);
+                break;
+            }
+            else
+            {
+                Texture2D tex = Managers.Resource.Load<Texture2D>("Textures/Cursors/Cursor_Hand");
+                Cursor.SetCursor(tex, new Vector2(tex.width / 3, 0), CursorMode.Auto);
+            }
+        }
+    }
+}

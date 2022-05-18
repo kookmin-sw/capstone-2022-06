f4c5b80fdda3984b65c4a9883710b069084b4f11
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-19 02:03:39 +0900

    [변경] 스킬 스프라이트를 가져오기 위한 초기 코드 작성
    스킬 스프라이트를 가져오기 위한 초기 코드 작성
    
    관련 이슈 : #125

--- Assets/Scripts/UI/SubItem/UI_ChampSkill.cs
+++ Assets/Scripts/UI/SubItem/UI_ChampSkill.cs
@@ -2,9 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+class SkillPaths
+{
+    public string passive, q, w, e, r;
+}
+
 public class UI_ChampSkill : UI_Base
 {
+    TextAsset skillIcons;
+
+    enum GameObjects
+    {
+        Passive,
+        Skill_Q,
+        Skill_W,
+        Skill_E,
+        Skill_R
+    }
+
     public override void Init()
     {
+        Bind<GameObject>(typeof(GameObjects));
+        string mySkillPath = (string)Util.GetLocalPlayerProp("skillPath");
+        skillIcons = Managers.Resource.Load<TextAsset>(mySkillPath);
     }
 }

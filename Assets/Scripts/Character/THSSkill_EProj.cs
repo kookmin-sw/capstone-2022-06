02521d32207d59ebf537f0c52ae6f26ef005ebc1
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-19 19:09:04 +0900

    [변경] E 스킬의 삭제가 PUN에 의해 제어되도록 변경
    E 스킬의 삭제가 PUN에 의해 제어되도록 변경
    
    관련 이슈 : #127

--- Assets/Scripts/Character/THSSkill_EProj.cs
+++ Assets/Scripts/Character/THSSkill_EProj.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class THSSkill_EProj : MonoBehaviour
 {
@@ -18,15 +19,21 @@ public class THSSkill_EProj : MonoBehaviour
     IEnumerator DestroyObject()
     {
         yield return new WaitForSeconds(0.55f);
-        Destroy(gameObject);
+        if (gameObject)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Enemy")
+        if(other.gameObject.layer == Util.GetEnemyLayer() && (other.tag == "Player" || other.tag == "Minion"))
         {
             other.GetComponent<Controller>().TakeDamage(damage, this.gameObject);
+            if (gameObject)
+            {
+                PhotonNetwork.Destroy(gameObject);
+            }
         }
     }
-
 }

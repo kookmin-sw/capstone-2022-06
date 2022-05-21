0ef8fcba5e890b1ace52b721c03f6531948c207f
HOGYEONG CHOE <devherdson@gmail.com>
2022-05-21 15:26:51 +0900

    [변경] UI_EndPopup 버그 수정
    버튼이 눌리지 않던 버그를 수정
    버튼을 누르면 LeaveRoom을  호출하도록 변경
    텍스트 변경이 잘 되지 않던 문제를 해결
    
    관련 이슈 : #139

--- Assets/Scripts/UI/SubItem/UI_EndPopup.cs
+++ Assets/Scripts/UI/SubItem/UI_EndPopup.cs
@@ -4,24 +4,52 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 
-public class UI_EndPopup : UI_Base
+public class UI_EndPopup : UI_Scene
 {
     enum Texts
     {
         ResultText
     }
 
+    enum Buttons
+    {
+        LobbyButton
+    }
+
     public override void Init()
     {
+        Managers.UI.SetCanvas(gameObject, true);
         Bind<Text>(typeof(Texts));
+        Bind<Button>(typeof(Buttons));
 
-        Util.SearchChild<Button>(gameObject).onClick.AddListener(() => {
-            PhotonNetwork.LoadLevel(0);
+        GetButton((int)Buttons.LobbyButton).onClick.AddListener(() => {
+            PhotonNetwork.LeaveRoom();
         });
     }
 
     public void SetResultText(string s)
     {
-        GetText((int)Texts.ResultText).text = s;
+        StartCoroutine(WaitBindTextCoroutine(s));
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.LoadLevel(0);
+    }
+
+    IEnumerator WaitBindTextCoroutine(string s)
+    {
+        yield return new WaitUntil(() => {
+            return GetText((int)Texts.ResultText) != null;
+        });
+
+        if (s == Util.GetMyLayerString())
+        {
+            GetText((int)Texts.ResultText).text = "패배";
+        }
+        else
+        {
+            GetText((int)Texts.ResultText).text = "승리";
+        }
     }
 }
